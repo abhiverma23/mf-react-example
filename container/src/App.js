ce03cb14a6fb69f9f9612b58a4f0c53e7cf4cf08
Abhishek Verma <www.abhiverma23@gmail.com>
2022-05-14 20:48:52 +0530

    fixed css issue for header and marketing for production

--- container/src/App.js
+++ container/src/App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+const criteria = createGenerateClassName({ productionPrefix: 'co' });
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={criteria}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 }
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 import NotFound from './components/NotFound';
 
+const criteria = createGenerateClassName({ productionPrefix: 'ma' });
+
 function App() {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={criteria}>
         <BrowserRouter>
           <Switch>
             <Route exact path='/pricing' component={Pricing} />
