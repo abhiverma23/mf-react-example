755848426a1fd5d79c1113dab13f5562f37e7967
Abhishek Verma <www.abhiverma23@gmail.com>
2022-05-14 20:01:50 +0530

    Added header in container

--- container/src/App.js
+++ container/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      <h1>Container App</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 }
 
